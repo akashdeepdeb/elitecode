6dbd173dcbd25ece265128606680c6fb69479cdd
Akashdeep Deb <deb2@illinois.edu>
2017-12-03 13:50:09 -0600

    added 344

---
+++ easy/344reverseString.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
+
+#maybe shortest code i've ever written 
