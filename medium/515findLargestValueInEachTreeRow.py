87c2fc55f7f6f18bfb56563053feb4eae61af033
Akashdeep Deb <deb2@illinois.edu>
2018-01-18 07:42:50 -0600

    new stuff

---
+++ medium/515findLargestValueInEachTreeRow.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def largestValues(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        if root is None:
+            return res
+        q = [root]
+        while q:
+            res.append(max(x.val for x in q))
+            new_q = []
+            for node in q:
+                if node.left:
+                    new_q.append(node.left)
+                if node.right:
+                    new_q.append(node.right)
+            q = new_q
+        return res
